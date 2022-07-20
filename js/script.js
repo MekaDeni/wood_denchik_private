722e4e04e5c7bce6c0043e847cba2839f92c4a84
mekadeni <deni.den.96@mail.ru>
2022-07-20 22:32:22 +0300

    начал делать карусель

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("В РАБОТЕ")
+/*______КАРУСЕЛЬ______*/
+const carousel = document.querySelector(".main__carousel"); // пишем блок карусели
+const carousel_button = document.getElementsByClassName("main__slaid-button"); // пишем массив кнопок слайдера
+
+// функция обработчик для кнопок слайдера
+function buttonCarouselEvent(e) {
+	if (e.target.classList[1] == "main__slaid-button--left") {
+		alert("Я ОП");
+	} else {
+		alert("Я БОБ");
+	}
+};
+
+for (let i = 0; carousel_button.length > 0; i++) {
+	carousel_button[i].onclick = buttonCarouselEvent;//пишем в цикле обработчик в каждую кнопку
+}
