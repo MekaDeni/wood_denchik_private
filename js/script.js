dd98d285e43ef17ac59213bdd50492a7af925ae4
mekadeni <deni.den.96@mail.ru>
2022-07-22 19:08:28 +0300

    доделал слайдер

--- js/script.js
+++ js/script.js
@@ -4,13 +4,44 @@ const carousel_button = document.getElementsByClassName("main__slaid-button"); /
 
 // функция обработчик для кнопок слайдера
 function buttonCarouselEvent(e) {
-	if (e.target.classList[1] == "main__slaid-button--left") {
-		alert("Я ОП");
-	} else {
-		alert("Я БОБ");
+	slaid_arr = carousel.getElementsByClassName("main__slaid"); // получаем массив слайов (HTML collection)
+	slaid_arr = Array.from(slaid_arr); // преабразуем в норм массив
+
+
+	if (e.target.classList[1] == "main__slaid-button--left") { // если нажата правая кнока
+		for (let i = slaid_arr.length - 1; i > 0; i--) { 
+			if (slaid_arr[i].classList[1] == "main__slaid--selected") { // если текущий элемент имеет класс --selected
+				if (i == 0) { // если итерация равна первому элементу массива
+					slaid_arr[i].classList.remove("main__slaid--selected");	
+					slaid_arr[slaid_arr.length - 1].classList.add("main__slaid--selected");
+					break;
+				}
+
+				slaid_arr[i].classList.remove("main__slaid--selected");
+				slaid_arr[i-1].classList.add("main__slaid--selected");
+				break;
+			} else if (slaid_arr[0].classList[1] == "main__slaid--selected") { // если первый элемент выбран (но не равен итерации (она равна длинне - 1))
+				slaid_arr[0].classList.remove("main__slaid--selected");
+				slaid_arr[slaid_arr.length - 1].classList.add("main__slaid--selected");
+			}
+		}
+	} else { // если нажата правая кнока
+		for (let i = 0; slaid_arr.length > i; i++) { 
+			if (slaid_arr[i].classList[1] == "main__slaid--selected") { // если текущий элемент имеет класс --selected
+				if (i == slaid_arr.length - 1) { // если итерация равна крайнему элементу массива
+					slaid_arr[i].classList.remove("main__slaid--selected");	
+					slaid_arr[0].classList.add("main__slaid--selected");
+					break;
+				}
+
+				slaid_arr[i].classList.remove("main__slaid--selected");
+				slaid_arr[i+1].classList.add("main__slaid--selected");
+				break;
+			}
+		}
 	}
 };
 
-for (let i = 0; carousel_button.length > 0; i++) {
-	carousel_button[i].onclick = buttonCarouselEvent;//пишем в цикле обработчик в каждую кнопку
+for (let i = 0; carousel_button.length > i; i++) {
+	carousel_button[i].addEventListener("click", buttonCarouselEvent); //пишем в цикле обработчик в каждую кнопку
 }
